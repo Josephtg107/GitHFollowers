ba8aea625f66e3338fd74d03bb4d196789a1d78d
Joseph <josephtg107@gmail.com>
2024-08-09 03:04:26 -0500

    [Build] Changed Navigation color to systemGreen added returnType to go on GHFieldText

---
+++ GitHFollowers/FollowerListVC.swift
@@ -0,0 +1,20 @@
+//
+//  FollowerListVC.swift
+//  GitHFollowers
+//
+//  Created by Joseph Garcia on 09/08/24.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
